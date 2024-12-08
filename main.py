898be5acb2c96a6952232d25f923458927dc1576
Heriec <2821188630@qq.com>
2024-12-08 19:13:15 +0800

    🐞 fix: 修复手动付款导致计算在内

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ if __name__ == '__main__':
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)   
     billList  = AliyunClient().billList(yesterday.strftime('%Y-%m'))
+
+    # 过滤掉没有UsageStartTime的数据
+    # 其他付款的数据不在这里统计
+    billList = filter(lambda x: x["UsageStartTime"] != '', billList)
+
     # json.dump(billList, open('query_bill.json', 'w'), indent=4)
     yesterdayBillList = [item for item in billList if datetime.datetime.strptime(item["UsageStartTime"], f"%Y-%m-%d %H:%M:%S").date() == yesterday]
     feishuClient = FeishuClient()
